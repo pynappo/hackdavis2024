c8865f485b9f6759a3501f0e3ab862b540efb901
pynappo <lehtien.david@gmail.com>
2024-04-28 02:56:02 -0700

    revert login change

--- hackdavis2/app/login/page.tsx
+++ hackdavis2/app/login/page.tsx
@@ -1,3 +1,34 @@
-export default function WelcomeMessage() {
-  return <div>This is a login page!</div>;
-}
+"use client";
+
+import {
+  useUser,
+  useRedirectFunctions,
+  useLogoutFunction,
+} from "@propelauth/nextjs/client";
+import { redirect } from "next/navigation";
+
+const WelcomeMessage = () => {
+  const { loading, user } = useUser();
+  const { redirectToLoginPage, redirectToAccountPage } = useRedirectFunctions();
+  const logoutFn = useLogoutFunction();
+
+  if (loading) return <div>Loading...</div>;
+
+  if (user) {
+    return (
+      <div>
+        <p>You are logged in as {user.email}</p>
+        <button onClick={() => redirectToAccountPage()}>Account</button>
+        <button onClick={logoutFn}>Logout</button>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <p>You are not logged in</p>
+        <button onClick={() => redirectToLoginPage()}>Login</button>
+      </div>
+    );
+  }
+};
+export default WelcomeMessage;
