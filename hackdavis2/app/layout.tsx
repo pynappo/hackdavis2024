195a0653c7d7d6c8180961ae688e75a68fbbda5b
pynappo <lehtien.david@gmail.com>
2024-04-27 19:36:22 -0700

    v2

---
+++ hackdavis2/app/layout.tsx
@@ -0,0 +1,15 @@
+import { AuthProvider } from "@propelauth/nextjs/client";
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <AuthProvider authUrl={process.env.NEXT_PUBLIC_AUTH_URL!}>
+        <body>{children}</body>
+      </AuthProvider>
+    </html>
+  );
+}
