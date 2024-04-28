bb9f3a971cd18b40e5f6f5da3f4c2459ace49f2f
damartialartirst <jackychenglhf@gmail.com>
2024-04-27 20:57:13 -0700

    button

--- hackdavis2/app/page.tsx
+++ hackdavis2/app/page.tsx
@@ -3,8 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <header className="">
-        <h2>Davis Housing </h2>
+      <header className="flex justify-between items-center bg-black">
+        <h2 className="font-bold p-5 text-white">
+          Davis Community Meals and Housing
+        </h2>
+        <a href="/welcome" className="mx-5">
+          <h2 className="px-2.5 py-1 text-black bg-white hover:bg-[#52525b] hover:text-white">
+            Login
+          </h2>
+        </a>
       </header>
     </main>
   );
