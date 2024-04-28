0d262cb838e57937a0971411ec56466ad126d744
damartialartirst <jackychenglhf@gmail.com>
2024-04-27 21:24:08 -0700

    admin dash

--- hackdavis2/app/(admin)/dashboard/create/page.tsx
+++ hackdavis2/app/(admin)/dashboard/create/page.tsx
@@ -13,6 +13,20 @@ export default function Create() {
   if (loading) return <div>Loading...</div>;
 
   if (user) {
+    return (
+      <main>
+        <input
+          className=" border-black border-2 rounded-md"
+          placeholder="insert item name"
+        ></input>
+        <input
+          className=" border-black border-2 rounded-md "
+          placeholder="number"
+        ></input>
+        <button className="p-3  border-black border-2">+</button>
+        <button className="p-3  border-black border-2">-</button>
+      </main>
+    );
   } else {
     redirectToLoginPage();
   }
