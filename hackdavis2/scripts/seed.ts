3ccda7b0c3b73fc27330ec61f882c6091188d401
pynappo <lehtien.david@gmail.com>
2024-04-27 19:36:22 -0700

    v2

---
+++ hackdavis2/scripts/seed.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+// Run inside `async` function
+async function main() {
+  const user = await prisma.item.create({
+    data: {
+      name: "Alice",
+      category: {
+        create: {
+          name: "Person",
+        },
+      },
+    },
+  });
+  console.log("hi");
+}
+
+await main();
